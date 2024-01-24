7dc1aa2fe909d5288b0ceb4c3108ccd94820196c
Yekaditya Devadi <yekaditya33@gmail.com>
2024-01-24 18:59:01 +0530

    Updates

---
+++ precache-manifest.b60d223e8c92fe270c369cf71c779818.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd870178be342e819a93872e9c97980",
+    "url": "/SnapShot/index.html"
+  },
+  {
+    "revision": "fdd26dbb468541d41b25",
+    "url": "/SnapShot/static/css/main.d03163bd.chunk.css"
+  },
+  {
+    "revision": "8ece58532b30179d084d",
+    "url": "/SnapShot/static/js/2.6b9f8f86.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SnapShot/static/js/2.6b9f8f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd26dbb468541d41b25",
+    "url": "/SnapShot/static/js/main.33fd0b86.chunk.js"
+  },
+  {
+    "revision": "17f44848bce1e5215d0e",
+    "url": "/SnapShot/static/js/runtime-main.136163d9.js"
+  }
+]);
